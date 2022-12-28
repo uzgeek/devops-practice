71aa62b6b160d189c8cab64486ba4343a0a68600
uzgeek <nigmatjon.aa@gmail.com>
2022-12-28 16:46:25 +0500

    grep is used

--- task4/script.sh
+++ task4/script.sh
@@ -6,10 +6,9 @@ for location in "Tashkent" "Moscow" "New-York"; do
     geo_url="http://api.openweathermap.org/geo/1.0/direct?q=$location&&appid=$API_KEY"
     latitude=$(curl "$geo_url" | jq '.[] | .lat')
     longitude=$(curl "$geo_url" | jq '.[] | .lon')
-    echo "$latitude"
 
     weather_url="http://www.7timer.info/bin/api.pl?lon=$longitude&lat=$latitude&product=astro&output=json"
-    weather=$(curl "$weather_url" | jq '.[] | .temp2m')
+    weather=$(curl -w -o "$weather_url" | grep temp2m)
 
-    echo $weather
+    echo $location $weather
 done
