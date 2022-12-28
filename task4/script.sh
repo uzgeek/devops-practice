a9d01018ad6b7fd671d1c9a5776a873f6af517ca
uzgeek <nigmatjon.aa@gmail.com>
2022-12-28 16:23:47 +0500

    api-key secreted

--- task4/script.sh
+++ task4/script.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+source .env
 for location in "Tashkent" "Moscow" "New-York"; do
 
-    geo_url="http://api.openweathermap.org/geo/1.0/direct?q=$location&&appid=3e924651582150c51ffce9692fbb9d90"
+    geo_url="http://api.openweathermap.org/geo/1.0/direct?q=$location&&appid=$API_KEY"
     latitude=$(curl "$geo_url" | jq '.[] | .lat')
     longitude=$(curl "$geo_url" | jq '.[] | .lon')
+    echo "$latitude"
 
     weather_url="http://www.7timer.info/bin/api.pl?lon=$longitude&lat=$latitude&product=astro&output=json"
     weather=$(curl "$weather_url" | jq '.[] | .temp2m')
